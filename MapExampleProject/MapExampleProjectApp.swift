f3be3c278d5baaf334033b90f1e6b22bbdfbd283
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2022-11-23 13:22:02 +0100

    Project init

---
+++ MapExampleProject/MapExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapExampleProjectApp.swift
+//  MapExampleProject
+//
+//  Created by Karina gurachevskaya on 23.11.22.
+//
+
+import SwiftUI
+
+@main
+struct MapExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
