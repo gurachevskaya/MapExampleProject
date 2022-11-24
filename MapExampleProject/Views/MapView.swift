8dd0cf6089ef6b8c5a15518e59ae8c7fb367471d
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2022-11-24 17:34:06 +0100

    Add clustering

--- MapExampleProject/Views/MapView.swift
+++ MapExampleProject/Views/MapView.swift
@@ -9,40 +9,63 @@ import MapKit
 import SwiftUI
 
 struct MapView: UIViewRepresentable {
+    
     var places: [PlaceAnnotation]
-
-    func makeCoordinator() -> MapViewCoordinator {
-        MapViewCoordinator(self)
-    }
     
-    func makeUIView(context: Context) -> MKMapView{
-        MKMapView(frame: .zero)
-    }
+//    @State private var region = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(
+//            latitude: 43.64422936785126, longitude: 142.39329541313924
+//        ),
+//        span: MKCoordinateSpan(latitudeDelta: 1.5, longitudeDelta: 2)
+//    )
     
-    func updateUIView(_ view: MKMapView, context: Context){
-        view.delegate = context.coordinator
+    class Coordinator: NSObject, MKMapViewDelegate {
         
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            guard let annotation = annotation as? PlaceAnnotation else { return nil }
+            return AnnotationView(annotation: annotation, reuseIdentifier: AnnotationView.reuseID)
+        }
+    }
+        
+    func makeCoordinator() -> Coordinator {
+        MapView.Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let view = MKMapView()
+        view.delegate = context.coordinator
+//        view.setRegion(region, animated: false)
+        view.mapType = .standard
         view.addAnnotations(places)
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
     }
 }
 
-class MapViewCoordinator: NSObject, MKMapViewDelegate {
+class AnnotationView: MKMarkerAnnotationView {
     
-    var mapViewController: MapView
-
-    init(_ control: MapView) {
-        self.mapViewController = control
+    static let reuseID = "annotation"
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        clusteringIdentifier = "clustering"
     }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotation")
-        
-        if annotationView == nil {
-            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
-            annotationView?.canShowCallout = true
-        }
-        
-        return annotationView
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForDisplay() {
+        super.prepareForDisplay()
+        displayPriority = .defaultLow
     }
 }
